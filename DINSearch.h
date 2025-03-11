64d3a6e71900f8c63138bde492b747a13fa5e0d4
zero-jeden <101535397+zero-jeden@users.noreply.github.com>
2025-03-11 09:10:15 -0400

    Update DINSearch.h

--- DINSearch.h
+++ DINSearch.h
@@ -1,1 +1,52 @@
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "extrafile.h"
+using namespace std;
 
+
+class Donor{
+    //DIN number and blood antigens
+    string _din;
+    string _C;
+    string _E;
+    string _c;
+    string _e;
+    string _CW;
+    string _V;
+    string _hrs;
+    string _VS;
+    string _hrB;
+    string _K;
+    string _k;
+    string _Kpa;
+    string _Kpb;
+    string _Jsa;
+    string _Jsb;
+    string _Jka;
+    string _Jkb;
+    string _Fya;
+    string _Fyb;
+    string _M;
+    string _N;
+    string _S;
+    string _s;
+    string _U;
+    string _Mia;
+    string _Dia;
+    string _Dib;
+    string _Doa;
+    string _Dob;
+    string _Hy;
+    string _Joa;
+    string _Coa;
+    string _Cob
+    string _Yta;
+    string _Ytb;
+    string _Lua;
+    string _Lub;
+    
+    
+}
